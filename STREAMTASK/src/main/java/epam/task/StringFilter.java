ab60a52d0ae8058884d70427be418f3cbb2b6630
Dhunde Vamshi Krishna <vamshik408@gmail.com>
2020-06-26 20:26:39 +0530

    Update StringFilter.java

--- STREAMTASK/src/main/java/epam/task/StringFilter.java
+++ STREAMTASK/src/main/java/epam/task/StringFilter.java
@@ -5,7 +5,7 @@ public class StringFilter {
 	List<String> sFilter(List<String> str)
 	{
 		return 
-		str.stream().filter(a.length()==3 && a->a.charAt(0)=='a')
+		str.stream().filter(a->a.length()==3 &&  a.charAt(0)=='a')
 		.collect(Collectors.toList());
 	}
 }
